5f431c482f68796ce949533b02411588e79838dd
junngo <myeongjun.ko@gmail.com>
2020-11-08 23:30:55 +0900

    change debug and allowed hosts value for deploy

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^^g4zx%pt7^0cyby6-rd^$^we5o3*a&bg=%_ic!4d#8ahcqr_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
